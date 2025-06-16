e54905c28def5ae2a1449ea0a44d78bf54241b43
e4mi <190270476+e4mi@users.noreply.github.com>
2025-06-16 18:59:25 +0000

    2025-06-16

--- os.c
+++ os.c
@@ -1,21 +1,41 @@
-extern void putchar(int c);
+extern void putchar(int);
 extern void clear(void);
 extern char getchar(void);
-extern void exit(int code);
+extern void exit(int);
 extern void *_malloc_here;
 extern const void *_malloc_max;
+
 typedef unsigned long size_t;
-#if defined(__x86_64__)
-typedef unsigned long uintptr_t;
-#else
-typedef unsigned int uintptr_t;
-#endif
+typedef __SIZE_TYPE__ Ref;
+enum { NIL = 0, TXT = 1, NUM = 3, FN = 5 };
 
 void print(char *s) {
-  while (*s) {
+  while (*s)
     putchar(*s++);
-  }
 }
+
+void *memcpy(void *dst, const void *src, size_t n) {
+  size_t i;
+  for (i = 0; i < n; i++)
+    ((char *)dst)[i] = ((char *)src)[i];
+  return 0;
+}
+
+int strncmp(const char *s1, const char *s2, size_t n) {
+  while (n-- > 0 && *s1 && *s1 == *s2)
+    s1++, s2++;
+  return *s1 - *s2;
+}
+size_t strlen(const char *s) {
+  int i = 0;
+  while (s[i])
+    i++;
+  return i;
+}
+int strcmp(const char *s1, const char *s2) {
+  return strncmp(s1, s2, strlen(s1));
+}
+
 void *malloc(size_t n) {
   void *r;
   n = (n + 3) & ~3;
@@ -27,98 +47,119 @@ void *malloc(size_t n) {
   _malloc_here = (void *)((size_t)_malloc_here + n + sizeof(size_t));
   return r;
 }
-typedef struct {
-  char type;
-} Value;
-typedef struct {
-  char type;
-  void *a, *b;
-} Pair;
-typedef struct {
-  char type;
-  int n;
-} Num;
-typedef struct {
-  uintptr_t t;
-  char *s;
-} Txt;
-typedef struct {
-  uintptr_t t;
-  void *(*f)(void **, void **);
-} Fn;
 
-enum { NIL = 0, TXT = 1, NUM = 3, FN = 5 };
-typedef uintptr_t Ref;
-typedef Ref Cell[2];
-Ref get(Ref x, int i) { return (Ref)((void **)x)[i]; }
-Ref set(Ref x, Ref a, Ref b) { return ((Ref *)x)[0] = a, ((Ref *)x)[1] = b, x; }
-char type(Ref x) { return (get(x, 0) & 1) ? get(x, 0) : NIL; }
-Ref mk(Ref a, Ref b) { return set((Ref)malloc(sizeof(Ref) * 2), a, b); }
-void print_hex(char *x, int n) {
-  int i = 0;
-  char *a = "0123456789abcdef";
-  for (i = 0; i < n; i++) {
-    putchar(a[x[i] >> 4 & 0xf]);
-    putchar(a[x[i] & 0xf]);
-  }
+size_t malloc_usable_size(void *ptr) { return ((size_t *)ptr)[-1]; }
+
+void free(void *ptr) { (void)ptr; }
+
+void *realloc(void *ptr, size_t n) {
+  void *r;
+  size_t m = malloc_usable_size(ptr);
+  if (m >= n)
+    return ptr;
+  n += 1024;
+  if (!ptr)
+    return malloc(n);
+  r = malloc(n);
+  memcpy(r, ptr, m);
+  free(ptr);
+  return r;
 }
+
+Ref get(Ref x, int i) { return ((Ref *)x)[i]; }
+Ref mk(Ref a, Ref b) {
+  Ref *x = (Ref *)malloc(sizeof(Ref) * 2);
+  x[0] = a;
+  x[1] = b;
+  return (Ref)x;
+}
+
+void print_hex(char *x, int n) {
+  int i;
+  static const char hex[] = "0123456789abcdef";
+  for (i = 0; i < n; i++)
+    putchar(hex[(x[i] >> 4) & 0xf]), putchar(hex[x[i] & 0xf]);
+}
+
 void print_dec(int x) {
-  if (x < 0) {
-    putchar('-');
-    x = -x;
-  }
-  if (x / 10) {
+  if (x < 0)
+    putchar('-'), x = -x;
+  if (x >= 10)
     print_dec(x / 10);
-  }
   putchar(x % 10 + '0');
 }
+
 void print_cell(Ref x) {
+  int t;
   char *s;
   if (!x) {
     print("nil");
     return;
-  } else if (type(x) == NIL) {
-    print("(");
-    print_cell(get(x, 0));
-    while (!type(x = get(x, 1))) {
-      putchar(' ');
-      print_cell(get(x, 0));
+  }
+  t = get(x, 0);
+  if (t == TXT) {
+    print("\"");
+    for (s = (char *)get(x, 1); *s; s++) {
+      if (*s == '"' || *s == '\\')
+        putchar('\\');
+      putchar(*s);
     }
-    if (!type(x)) {
-      print(" . ");
-      print_cell(x);
-    }
-    putchar(')');
+    print("\"");
+  } else if (t == NUM) {
+    print_dec(get(x, 1));
   } else {
-    int t = type(x);
-    print("\n");
-    if (t == TXT) {
-      print("\"");
-      for (s = (char *)get(x, 1); *s; s++) {
-        if (*s == '"' || *s == '\\') {
-          putchar('\\');
-        }
-        putchar(*s);
-      }
-      print("\"");
-    } else if (t == NUM) {
-      print_dec(get(x, 1));
-    } else {
-      print("#");
-      print_hex((char *)x, sizeof(Ref));
-      print(" #");
-      print_hex((char*)x + sizeof(Ref), sizeof(Ref));
-    }
+    print("#"), print_hex((char *)x, sizeof(Ref)*2);
   }
 }
 
+/* editable input line */
+char *readline(char **line) {
+  int i = 0;
+  int size = 0;
+  char c;
+  for (;;) {
+    if (i + 2 >= size) {
+      realloc(*line, size += 128);
+    }
+    c = getchar();
+    if (c == '\b' || c == 0x7F) {
+      if (i > 0) {
+        (*line)[i--] = 0;
+        putchar('\b');
+        putchar(' ');
+        putchar('\b');
+      }
+    }
+    if (c >= ' ' && c <= '~') {
+      (*line)[i++] = c;
+      putchar(c);
+    }
+    if (c == '\n' || c == 0) {
+      (*line)[i] = 0;
+      break;
+    }
+  }
+  return *line;
+}
 int main(void) {
-  Ref x = 0;
+  char *line = 0;
   clear();
   print("\n _^..^_ meow!\n\n");
   print_cell(mk(NUM, 42));
   print("\n");
-  print_cell(mk(TXT, (Ref)"meow"));
+  print_cell(mk(TXT, (Ref) "meow"));
   print("\n");
+  while (1) {
+    print("> ");
+    readline(&line);
+    print("\n");
+    if (strcmp(line, "exit") == 0)
+      break;
+    else
+      print_cell(mk(TXT, (Ref) line));
+    print("\n");
+  }
+  print("byeeeee...\n");
+  free(line);
   return 0;
 }
